975daeb732b6c11d4ade56266107db507170e7ec
Jaideep Bhoosreddy <jaideep.coder@gmail.com>
2014-11-30 00:47:17 +0530

    module update: khaas

--- modules/khaas/controllers/front/days.php
+++ modules/khaas/controllers/front/days.php
@@ -1,12 +1,52 @@
 <?php
+
+if (!defined('_PS_VERSION_'))
+    exit;
+
 class khaasDaysModuleFrontController extends ModuleFrontController
 {
 
-public function __construct()
-{
-    parent::__construct();
-    $this->context = Context::getContext();
-    
-}
-
+	public function __construct()
+	{
+	    parent::__construct();
+	    $this->context = Context::getContext();
+	    
+	}
+	
+	public function init()
+	{
+		$this->page_name = 'khaasdays'; // page_name and body id
+		$this->display_column_left = TRUE;
+		//$this->display_column_right = TRUE;
+	    parent::init();
+	}
+	
+	public function initContent()
+	{
+	    parent::initContent();
+		$products_count = $this->module->countAllProducts();
+		$this->pagination($products_count);
+		$products = $this->module->getActiveProducts();/*
+	    $products_partial = Product::getProducts($this->context->language->id, ((int)$this->p - 1) * (int)$this->n, $this->n, 'name', 'asc'); 
+		$products = Product::getProductsProperties($this->context->language->id, $products_partial);
+		/* Retrieving product images *//*
+ 
+		foreach ($products as $key => $product) {
+    		foreach ($products as $key => $product) {
+	        	$products[$key]['id_image'] = Product::getCover($product['id_product'])['id_image'];
+		    }
+		}*/
+		$this->context->smarty->assign(array(
+			'products' => $products,
+			'homeSize' => ImageCore::getSize('home_default')
+		));
+		$this->setTemplate('days.tpl');
+	}
+	
+	public function setMedia()
+	{
+	    parent::setMedia();
+	    $this->addCSS(__PS_BASE_URI__.'modules/'.$this->module->name.'/css/'.$this->module->name.'.css');
+		$this->context->controller->addJS(__PS_BASE_URI__.'modules/'.$this->module->name.'/js/'.$this->module->name.'.js');
+	}   
 }
--- modules/khaas/khaas.php
+++ modules/khaas/khaas.php
@@ -7,7 +7,8 @@ class khaas extends Module
 {
     /* @var boolean error */
     protected $_errors = false;
-
+	private $_html = '';
+	
     public function __construct()
     {
         $this->name = 'khaas';
@@ -17,7 +18,7 @@ class khaas extends Module
         $this->need_instance = 0;
 
         parent::__construct();
-
+		$this->context = Context::getContext();
         $this->displayName = $this->l('Khaas Module');
         $this->description = $this->l('Adds a block.');
 	$this->confirmUninstall = $this->l('Are you sure you want to delete this module?');
@@ -26,7 +27,7 @@ class khaas extends Module
     public function install() {
 
         if (!parent::install())
-	    return false;
+	    	return false;
         return true;
     }
 
@@ -36,6 +37,138 @@ class khaas extends Module
             return false;
         return true;
     }
+	
+	public function getContent()
+	{
+		
+		if (Tools::isSubmit('submit'))
+	  	{
+	  		$toast = "";
+	  		if(Tools::getValue('type') == 'add') {
+	  			$this->addProduct(Tools::getValue('product'));
+	  			$toast = "Product added successfully.";
+	  		}
+			elseif(Tools::getValue('type') == 'remove') {
+				$this->removeProduct(Tools::getValue('id_product'));
+				$toast = "Product removed successfully.";
+			}
+	  		$this->_html .= '<div class="bootstrap">
+		<div class="alert alert-success">
+			<button type="button" class="close" data-dismiss="alert">×</button>
+			'.$toast.'
+		</div>
+	</div>';
+	  		
+			
+	    	//Configuration::updateValue($this->name.'_message', Tools::getValue('our_message'));
+	  	}
+	  	$this->_displayForm();
+	  	return $this->_html;
+	}
+	
+	public function _displayForm()
+	{
+		$_options = "";
+		$products_count = $this->countAllProducts();
+		//$this->_html .= "Product Count : ".$products_count.'<br>';
+		$products = Product::getProducts($this->context->language->id, 0, $products_count, 'name', 'asc');
+		$ids = $this->getProducts();
+		$id_list = array();
+		foreach ($ids as $id) {
+			array_push($id_list, $id['id_product']);
+		}
+		$activeBool = "";
+		foreach ($products as $product) {
+			if($this->isActiveProduct($product['id_product'], $id_list)) {
+				$activeBool = "disabled";
+			}
+			else {
+				$activeBool = "";
+			}
+			$_options .= '<option value="'.$product['id_product'].'" '.$activeBool.'>'.$product['name'].'</option>';
+		}
+		//$this->_html .= serialize($products);
+		$this->_html .= '<h1>Khaas Modules</h1>
+						<hr><h2>Khaas Days</h2>
+							<h3>Product List</h3>
+						    <form action="'.$_SERVER['REQUEST_URI'].'" method="post">
+	      					<label>'.$this->l('Select Product').'</label>
+	      					<div class="margin-form">
+	        				<select name="product">'.$_options.'</select>
+	      					</div>
+	      					<input type="hidden" name="type" value="add" />
+	      					<input type="submit" name="submit" value="'.$this->l('Submit').'" class="button" />
+	    				</form>';
+		
+		$active = $this->getProducts();
+		//$this->_html .= serialize($active);
+		$this->_html .= '<h3>Active Products</h3><ol>';
+		foreach ($active as $product) {
+			$this->_html .= '<li><form action="'.$_SERVER['REQUEST_URI'].'" method="post"><input type="hidden" name="type" value="remove" />
+			<input type="hidden" name="id_product" value="'.$product['id_product'].'" />
+			<input type="submit" name="submit" value="'.$this->l('Remove').'" class="button" /><b>&nbsp&nbsp'.ProductCore::getProductName($product['id_product']).'</b></li></form>';
+		}
+		$this->_html .= '</ol>';
+	}
+	
+	public function countAllProducts()
+	{
+    	return Db::getInstance()->getValue('SELECT COUNT(*) from ps_product');
+	}
+	
+	public function addProduct($id)
+	{
+		$data = array('id_product' => (int)$id);
+		return Db::getInstance()->execute('INSERT INTO `ps_khaas_days` (`id_product`) VALUES ("'.$id.'")');
+		//return Db::getInstance()->insert('ps_khaas_days', $data);
+	}
+
+	public function removeProduct($id)
+	{
+		$data = array('id_product' => (int)$id);
+		return Db::getInstance()->execute('DELETE FROM `ps_khaas_days` WHERE `id_product` = '.$id);
+	}
+	
+	public function getProducts()
+	{
+		return Db::getInstance()->executeS('SELECT * FROM `ps_khaas_days` ');
+	}
+	
+	public function getActiveProducts()
+	{
+		$ids = $this->getProducts();
+		$id_list = array();
+		foreach ($ids as $id) {
+			array_push($id_list, $id['id_product']);
+		}
+		
+		$products_partial = Product::getProducts($this->context->language->id, ((int)$this->p - 1) * (int)$this->n, $this->n, 'name', 'asc'); 
+		$products = Product::getProductsProperties($this->context->language->id, $products_partial);
+		/* Retrieving product images */
+ 
+		foreach ($products as $key => $product) {
+    		foreach ($products as $key => $product) {
+	        	$products[$key]['id_image'] = Product::getCover($product['id_product'])['id_image'];
+		    }
+		}
+		$active_product = array();
+		foreach ($products as $product) {
+			if($this->isActiveProduct($product['id_product'], $id_list)) {
+				array_push($active_product, $product);
+			}
+		}
+		return $active_product;
+	}
+	
+	public function isActiveProduct($id, $ids)
+	{
+		if(in_array($id, $ids)) {
+			return TRUE;
+		}
+		else {
+			return FALSE;
+		}
+	}
 }
 
 
